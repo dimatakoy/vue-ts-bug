3a7b9f4a0c0c99d1a75505448a6480fdef02647c
Dmitry Vakhnenko <dv@Dmitrys-MacBook-Air.local>
2023-05-11 20:01:09 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import MyButton from './MyButton.vue';
+</script>
+
+<template>
+  <MyButton variant="blue" />
+</template>
---
+++ src/MyButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button>
+        <slot />
+    </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from 'vue'
+
+// Ths code will throw error
+
+interface MyButtonProps extends ButtonHTMLAttributes {
+    variant: 'blue' | 'white'
+}
+
+/*
+error during build:
+Error: [@vue/compiler-sfc] Failed to resolve index type into finite keys
+
+___masked__/vue-ts-bug/node_modules/@vue/runtime-dom/dist/runtime-dom.d.ts
+1258|  }
+1259|  type EventHandlers<E> = {
+1260|      [K in keyof E]?: E[K] extends (...args: any) => any ? E[K] : (payload: E[K]) => void;
+   |            ^^^^^^^
+1261|  };
+1262|  
+*/
+
+defineProps<MyButtonProps>()
+</script>
